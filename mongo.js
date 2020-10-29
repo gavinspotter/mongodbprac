bc8aa9f2f3bcfc3b9aae810ed642aa6ae3f523de
Osiris <potterosiris@gmail.com>
2020-10-29 19:14:01 -0400

    mongo.js

--- mongo.js
+++ mongo.js
@@ -1,9 +1,26 @@
 const MongoClient = require("mongodb").MongoClient;
 
 const url =
-  "mongodb+srv://gavin:Kestral123@cluster0.d3tnt.mongodb.net/products?retryWrites=true&w=majority";
+  "mongodb+srv://gavin:Kestral123@cluster0.d3tnt.mongodb.net/products_test?retryWrites=true&w=majority";
 
-const createProduct = async (req, res, next) => {};
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "could not store data" });
+  }
+  client.close();
+
+  res.json(newProduct);
+};
 
 const getProducts = async (req, res, next) => {};
 
