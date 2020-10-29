1e36602c6743701119a959e6127ed3c464eb933a
Osiris <potterosiris@gmail.com>
2020-10-29 19:06:30 -0400

    mongo url & password

--- mongo.js
+++ mongo.js
@@ -1,3 +1,8 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url =
+  "mongodb+srv://gavin:Kestral123@cluster0.d3tnt.mongodb.net/products?retryWrites=true&w=majority";
+
 const createProduct = async (req, res, next) => {};
 
 const getProducts = async (req, res, next) => {};
