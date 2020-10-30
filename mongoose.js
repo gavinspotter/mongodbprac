c8116f7bd4eb05a465165b7c427caf705aabda9f
Osiris <potterosiris@gmail.com>
2020-10-29 20:05:47 -0400

    get products mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -2,11 +2,32 @@ const mongoose = require("mongoose");
 
 const Product = require("./models/product");
 
+mongoose
+  .connect(
+    "mongodb+srv://gavin:Kestral123@cluster0.d3tnt.mongodb.net/products_test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch(() => {
+    console.log("connection failed");
+  });
+
 const createProduct = async (req, res, next) => {
   const createdProduct = new Product({
     name: req.body.name,
     price: req.body.price,
   });
+  const result = await createdProduct.save();
+  //console.log(typeof createdProduct.id);
+
+  res.json(result);
+};
+
+const getProducts = async (req, res, next) => {
+  const products = await Product.find().exec();
+  res.json(products);
 };
 
 exports.createProduct = createProduct;
+exports.getProducts = getProducts;
